52b8e5f957e1ae510e22280186893cde1b06bb0d
salehibneomar <salehibneomar@gmail.com>
2025-06-05 23:18:11 +0600

    modifed UserRole enum, User migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
-            $table->enum('role', UserRole::values());
+            $table->enum('role', UserRole::allRoles());
             $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
