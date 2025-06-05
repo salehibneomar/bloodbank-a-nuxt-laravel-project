20eae13dec765e0968b7f4b826dc03db70980c69
salehibneomar <salehibneomar@gmail.com>
2025-06-05 23:08:07 +0600

    modifed user model, migration, added userole enum

---
+++ backend/app/Enums/UserRole.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+enum UserRole: string
+{
+    case Admin = 'admin';
+    case Donor = 'donor';
+    case Visitor = 'visitor';
+
+    public static function values(): array
+    {
+        return array_map(fn(self $role) => $role->value, self::cases());
+    }
+
+}
+
+
+
