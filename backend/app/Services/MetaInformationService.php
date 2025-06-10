6e3fd822341417a8c5e2833ce8415741e449448b
salehibneomar <salehibneomar@gmail.com>
2025-06-10 21:07:22 +0600

    implemented the MetaInformation service.controller.api to get the admin roles and mapped blood-groups also added them to cache for 7 days, fixed the filter by blood group not working and automated the is_available column of donor_information when user inputs last_donation_date which is restricted to current_date using the rules: UPDATED

--- backend/app/Services/MetaInformationService.php
+++ backend/app/Services/MetaInformationService.php
@@ -11,6 +11,7 @@ class MetaInformationService
     {
         $cacheKey = 'meta:information';
         $cacheTag = AdminCache::META_TAG->value;
+        $cacheDuration = 7 * 24 * 60 * 60;
 
         $data = cache()->tags($cacheTag)->get($cacheKey);
 
@@ -19,8 +20,8 @@ class MetaInformationService
                 'blood_groups' => BloodGroup::mappedBloodGroup(),
                 'user_roles' => UserRole::allRoles(),
             ];
-            dispatch(function () use ($cacheTag, $cacheKey, $data) {
-                cache()->tags($cacheTag)->put($cacheKey, $data, AdminCache::invalidateTime());
+            dispatch(function () use ($cacheTag, $cacheKey, $data, $cacheDuration) {
+                cache()->tags($cacheTag)->put($cacheKey, $data, $cacheDuration);
             });
         }
 
