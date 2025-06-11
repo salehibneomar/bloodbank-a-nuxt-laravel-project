d181a85f056185094f6e68a85dc1de98fef7e207
salehibneomar <salehibneomar@gmail.com>
2025-06-11 17:24:37 +0600

    added nuxt as the FE framework, added pinia, pinia-persisted-state, secure-ls, quasar-ui, axios, ande made modifications to the nuxt.conf.ts

---
+++ frontend/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const todos = ref([])
+
+onMounted(async () => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/todos')
+  todos.value = res.data
+})
+</script>
+<template>
+    <div>
+        <q-btn color="primary" icon="check" label="OK" />
+        <ul>
+          <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+        </ul>
+    </div>
+</template>
